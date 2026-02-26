79b047c30188477c41ce9d0f3067ccaa4686a0b4
dianerdiana <dianerdiana.de@gmail.com>
2026-02-26 15:45:51 +0700

    Check request url

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -8,6 +8,12 @@ describe('Login Page (E2E)', () => {
 
   beforeEach(() => {
     cy.visit('/login');
+
+    cy.intercept('POST', '**/*').as('anyPost');
+    cy.wait('@anyPost').then((i) => {
+      // lihat i.request.url
+      cy.log(i.request.url);
+    });
   });
 
   it('should render login page correctly', () => {
@@ -44,18 +50,18 @@ describe('Login Page (E2E)', () => {
       statusCode: 200,
       body: {
         status: 'success',
-        message: 'Login success',
+        message: 'ok',
         data: { token: 'fake-jwt-token' },
       },
     }).as('loginRequest');
 
-    cy.get(emailInput).type('example@dicoding.com');
-    cy.get(passwordInput).type('password123');
+    cy.get(emailInput).type('de.01@dianerdiana.com');
+    cy.get(passwordInput).type('123456');
     cy.contains('button', 'Login').click();
 
     cy.wait('@loginRequest').its('request.body').should('deep.include', {
-      email: 'example@dicoding.com',
-      password: 'password123',
+      email: 'de.01@dianerdiana.com',
+      password: '123456',
     });
 
     cy.location('pathname').should('eq', '/');
@@ -90,15 +96,15 @@ describe('Login Page (E2E)', () => {
           statusCode: 200,
           body: {
             status: 'success',
-            message: 'Login success',
+            message: 'ok',
             data: { token: 'fake-jwt-token' },
           },
         });
       });
     }).as('slowLoginRequest');
 
-    cy.get(emailInput).type('example@dicoding.com');
-    cy.get(passwordInput).type('password123');
+    cy.get(emailInput).type('de.01@dianerdiana.com');
+    cy.get(passwordInput).type('123456');
     cy.contains('button', 'Login').click();
 
     cy.contains('button', 'Login').should('be.disabled');
