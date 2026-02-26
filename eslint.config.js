8be26a9b35815e3aaf8badea1aec20ea6de1fb76
dianerdiana <dianerdiana.de@gmail.com>
2026-02-26 18:27:49 +0700

    Update storybook eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,4 @@
 // For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from "eslint-plugin-storybook";
-
 import js from '@eslint/js';
 import tsPlugin from '@typescript-eslint/eslint-plugin';
 import tsParser from '@typescript-eslint/parser';
@@ -12,70 +10,75 @@ import reactPlugin from 'eslint-plugin-react';
 import reactHooksPlugin from 'eslint-plugin-react-hooks';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
+import storybook from 'eslint-plugin-storybook';
 import { defineConfig } from 'eslint/config';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
-export default defineConfig([// Basis style Dicoding
-daStyle, // Rules tambahan untuk React + TS
-{
-  files: ['**/*.{ts,tsx}'],
-  extends: [
-    js.configs.recommended,
-    tseslint.configs.recommended,
-    reactHooks.configs.flat.recommended,
-    reactRefresh.configs.vite,
-    pluginCypress.configs.recommended,
-  ],
-  languageOptions: {
-    parser: tsParser,
-    parserOptions: {
-      project: ['./tsconfig.app.json', './tsconfig.node.json'],
-      tsconfigRootDir: import.meta.dirname,
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-      ecmaFeatures: { jsx: true },
-    },
-    globals: globals.browser,
-  },
-  plugins: {
-    '@typescript-eslint': tsPlugin,
-    react: reactPlugin,
-    'react-hooks': reactHooksPlugin,
-    import: importPlugin,
-  },
-  settings: {
-    react: {
-      version: 'detect',
-    },
-    'import/resolver': {
-      typescript: {},
-    },
-  },
-  rules: {
-    // Import
-    'import/no-extraneous-dependencies': [
-      'error',
-      {
-        devDependencies: ['vite.config.*', '**/*.config.*', '**/scripts/**', '**/*.test.*'],
-      },
+export default defineConfig([
+  // Basis style Dicoding
+  daStyle, // Rules tambahan untuk React + TS
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+      pluginCypress.configs.recommended,
     ],
-
-    // Function component style
-    'react/function-component-definition': [
-      'error',
-      {
-        namedComponents: 'arrow-function',
-        unnamedComponents: 'arrow-function',
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: ['./tsconfig.app.json', './tsconfig.node.json'],
+        tsconfigRootDir: import.meta.dirname,
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: { jsx: true },
       },
-    ],
+      globals: globals.browser,
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+      react: reactPlugin,
+      'react-hooks': reactHooksPlugin,
+      import: importPlugin,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+      'import/resolver': {
+        typescript: {},
+      },
+    },
+    rules: {
+      // Import
+      'import/no-extraneous-dependencies': [
+        'error',
+        {
+          devDependencies: ['vite.config.*', '**/*.config.*', '**/scripts/**', '**/*.test.*', '.storybook/*.*'],
+        },
+      ],
 
-    '@typescript-eslint/no-explicit-any': 'off',
+      // Function component style
+      'react/function-component-definition': [
+        'error',
+        {
+          namedComponents: 'arrow-function',
+          unnamedComponents: 'arrow-function',
+        },
+      ],
+
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
   },
-}, {
-  files: ['src/components/ui/**/*.{ts,tsx}'],
-  rules: {
-    'react-refresh/only-export-components': 'off',
-  },
-}, // Prettier is typically placed last
-prettier, ...storybook.configs["flat/recommended"]]);
+  {
+    files: ['src/components/ui/**/*.{ts,tsx}'],
+    rules: {
+      'react-refresh/only-export-components': 'off',
+    },
+  }, // Prettier is typically placed last
+  prettier,
+  ...storybook.configs['flat/recommended'],
+]);
